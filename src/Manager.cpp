889e2372a0bed185e820625ffd3eb0b7c5f2c212
Simonlovald <simonlovald@gmail.com>
2020-12-14 23:19:46 +0100

    Test pass or2(a,a)

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -167,7 +167,7 @@ BDD_ID Manager::or2(const BDD_ID a, const BDD_ID b)
                 return 1;
             else
                 return a;
-   // else return a;
+    else return a;
        
             
 }
