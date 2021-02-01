e678e713a7c32aec64136cce83d2ea9630ffa91a
Luiza <scorrea@rhrk.uni-kl.de>
2021-02-01 21:51:42 +0100

    reachable

---
+++ src/reachable/Reachable.cpp
@@ -0,0 +1,6 @@
+//
+// Created by ludwig on 27.11.18.
+//
+
+#include "Reachable.h"
+
---
+++ src/reachable/Reachable.h
@@ -0,0 +1,20 @@
+//
+// Created by ludwig on 27.11.18.
+//
+
+#ifndef VDSPROJECT_IMGCOMP_H
+#define VDSPROJECT_IMGCOMP_H
+
+
+#include <map>
+#include "ReachableInterface.h"
+
+namespace ClassProject {
+
+    class Reachable : public ReachableInterface {
+
+
+    };
+
+}
+#endif //VDSPROJECT_IMGCOMP_H
